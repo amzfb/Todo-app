efe659259c4a170950028c2fed13d86421204486
Aaaaaaaakihiro <youhaveanewworld@gmail.com>
2020-01-03 19:26:25 +0000

    フッターをmaterial-uiで装飾

--- todo-app/src/components/Footer.js
+++ todo-app/src/components/Footer.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import FilterLink from '../containers/FilterLink'
 import { VisibilityFilters } from '../actions'
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
 
 const Footer = () => (
-  <p>
-    Show: <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
-    {', '}
-    <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
-    {', '}
-    <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
-  </p>
+  <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+    <Button>
+      <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
+    </Button>
+    <Button>
+      <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
+    </Button>
+    <Button>
+      <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
+    </Button>
+  </ButtonGroup>
 )
 
 export default Footer
